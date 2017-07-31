6d1428ac74cfb27fce09ef41195b615f35251986
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-31 17:25:48 +0200

    🌲🍉 🍇 🍉🍈🍊 🍌🍅🍓🍒🍑 🍏

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,14 +3,23 @@ import {getFunName} from '../helpers'
 
 
 class StorePicker extends React.Component {
+goToStore(){
+//Attraper le text
+//copier dans l'url
+
+console.log('you Changed the URl');
+
+
+}
+
 	render(){
 		// return React.createElement('p',{className:'Testing'},' I love You')
 		return (
-		<form className="store-selector">
+		<form className="store-selector" onSubmit={this.goToStore}>
 	{/*POur cOMMENTER */}
 		<h2>Please Enter a Store</h2>
-		<input type="text" required placeHolder="Store Name" defaultValue={getFunName()}></input>
-		<button type="submit">visit Store </button>
+		<input type="text" required placeholder="Store Name" defaultValue={getFunName()}></input>
+		<button type="submit">Visit the Store </button>
 		</form>
 		)
 	}
